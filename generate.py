4af2ff0059476edcb21fc1c69e8aa73bdf3977a9
Vladimir Keleshev <halst@ya.ru>
2012-10-03 20:51:45 +0200

    Make generate.py use python instead of python3

--- generate.py
+++ generate.py
@@ -1,9 +1,9 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 from markdown import markdown
 
-template = open('./template.html').read()#.decode('utf8')
-index = open('./index.md').read()#.decode('utf8')
+template = open('./template.html').read().decode('utf8')
+index = open('./index.md').read().decode('utf8')
 output = open('./index.html', 'w')
 
 html = markdown(index, extras=[])
-output.write(template.replace('{body}', html))
+output.write(template.replace('{body}', html).encode('utf8'))
